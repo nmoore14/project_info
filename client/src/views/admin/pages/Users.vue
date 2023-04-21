43fe270fa8619f2faf07e5de8850d0cc1971f8c5
Nick Moore <nmoore@rushmarket.com>
2023-04-21 17:05:14 -0400

    Adding a user table for displaying admin users

--- client/src/views/admin/pages/Users.vue
+++ client/src/views/admin/pages/Users.vue
@@ -1,5 +1,68 @@
 <template>
-  <h1>Users</h1>
+  <div>
+    <h1>Users</h1>
+    <div class="overflow-x-auto w-full">
+      <table class="table w-full">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" class="checkbox" />
+              </label>
+            </th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr>
+            <th>
+              <label>
+                <input type="checkbox" class="checkbox" />
+              </label>
+            </th>
+            <td>
+              <div class="flex items-center space-x-3">
+                <div class="avatar">
+                  <div class="mask mask-squircle w-12 h-12">
+                    <img src="https://lh3.googleusercontent.com/a/AGNmyxZw4eyL3ml8Cv23O0rM3jHflY9CqqqnxzQ9ZxgH=s96-c" alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <div class="font-bold">Hart Hagerty</div>
+                  <div class="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br />
+              <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+            </td>
+            <td>Purple</td>
+            <th>
+              <button class="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+        </tbody>
+        <!-- foot -->
+        <tfoot>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th></th>
+          </tr>
+        </tfoot>
+
+      </table>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
